a7dedb2bf93c7c71ce9537f8e6cd1f4340bfaeaa
Nomis010 <143756368+Nomis010@users.noreply.github.com>
2026-05-12 19:23:20 +0200

    Change import from App.module.css to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { loadData, saveData, KEYS } from './storage.js';
-import styles from './App.module.css';
+import styles from './index.css';
 
 const ADMIN_USERNAME = 'admin';
 const ADMIN_PASSWORD = 'admin123';
