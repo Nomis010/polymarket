af5d1ffe51d6510ffed13d1ad183d6d3284dd4f1
Nomis010 <143756368+Nomis010@users.noreply.github.com>
2026-05-12 19:29:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { loadData, saveData, KEYS } from './storage.js';
-import './index.css';
+import styles from './index.css';
+
 
 const ADMIN_USERNAME = 'admin';
 const ADMIN_PASSWORD = 'admin123';
