f0ed2df0b28b2af380a238ac8ee40773940e1bfd
Nomis010 <143756368+Nomis010@users.noreply.github.com>
2026-05-14 11:16:41 +0200

    Delete api directory

--- api/state.js
+++
@@ -1,6 +0,0 @@
-import { getData, publicState, send } from './_store.js';
-
-export default async function handler(req, res) {
-  const data = await getData();
-  return send(res, publicState(data));
-}
