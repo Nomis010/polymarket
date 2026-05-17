870501f6c2733dbe8e7fd494660964c95b57d95d
Nomis010 <143756368+Nomis010@users.noreply.github.com>
2026-05-17 14:33:53 +0200

    Delete api directory

--- api/auth/me.js
+++
@@ -1,7 +0,0 @@
-import { readToken, send } from '../_store.js';
-
-export default async function handler(req, res) {
-  const user = readToken(req);
-  if (!user) return send(res, { error: 'Session invalide' }, 401);
-  return send(res, { user });
-}
